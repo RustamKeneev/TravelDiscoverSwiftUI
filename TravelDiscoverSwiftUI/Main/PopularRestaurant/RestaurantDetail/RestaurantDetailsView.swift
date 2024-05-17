d01ef712b2e6697bb55831f94755da899cc4c275
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-05-18 03:38:17 +0600

    added bussines logic display random star

--- TravelDiscoverSwiftUI/Main/PopularRestaurant/RestaurantDetail/RestaurantDetailsView.swift
+++ TravelDiscoverSwiftUI/Main/PopularRestaurant/RestaurantDetail/RestaurantDetailsView.swift
@@ -9,62 +9,68 @@ import SwiftUI
 import Kingfisher
 
 struct RestaurantDetailsView: View {
-    //MARK: - PROPERTIES
+    // MARK: - PROPERTIES
     @ObservedObject var vm: RestaurantDetailsViewModel
 
     let restaurant: Restaurant
-    
-    init(restaurant: Restaurant){
+    @State private var randomStarCount: Float = Float.random(in: 1...5)
+
+    init(restaurant: Restaurant) {
         self.restaurant = restaurant
         self.vm = .init(id: restaurant.id)
     }
 
     var body: some View {
-        ScrollView{
-            ZStack(alignment: .bottomLeading){
+        ScrollView {
+            ZStack(alignment: .bottomLeading) {
                 Image(restaurant.imageName)
                     .resizable()
                     .scaledToFill()
+                
                 LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .center, endPoint: .bottom)
-                HStack{
+                HStack {
                     VStack(alignment: .leading, spacing: 8) {
                         Text(restaurant.name)
                             .foregroundColor(.white)
                             .font(.system(size: 18, weight: .bold))
                         HStack {
-                            ForEach(0..<5,id: \.self){ num in
-                                Image(systemName: "star.fill")
-                            }//: LOOP
-                            .foregroundColor(.orange)
+                            StarsView(rating: CGFloat(randomStarCount), maxRating: 5)
+                            let number = randomStarCount
+                            let formattedNumber = String(format: "%.2f", number)
+                            Text("Rating: \(String(format: "%.2f", randomStarCount))")
+                                .foregroundColor(Color.yellow)
+                                .font(.system(size: 18, weight: .bold))
                         }//: HSTACK
                     }//: VSTACK
                     Spacer()
-                    NavigationLink {
-                        RestaurantPhotosView(vm: vm)
+                    NavigationLink {RestaurantPhotosView(vm: vm)
                     } label: {
-                        Text("See more photos")
+                        Text("See more \(vm.restaurant?.photos.count ?? 0) photos")
                             .foregroundColor(.white)
                             .font(.system(size: 14, weight: .regular))
                             .frame(width: 80)
                             .multilineTextAlignment(.trailing)
-                    }//: NAVIGATION LINK
+                    }//: NAVLINK
                 }//: HSTACK
                 .padding()
             }//: ZSTACK
-
             LocationDescriptionView(vm: vm)
             if let dishes = vm.restaurant?.popularDishes {
                 PopularDishesView(dishes: dishes)
-            }
-
+            }//: IF LET
             if let reviews = vm.restaurant?.reviews {
                 CustomerReviewsVIew(reviews: reviews)
-            }//: IF LET 
+            }//: IF LET
         }//: SCROLLVIEW
         .navigationBarTitle("Restaurant Details", displayMode: .inline)
-    }
+        .onAppear {
+            randomStarCount = Float.random(in: 1...5)
+            print(randomStarCount)
+        }//: ON APPEAR
+    }//: END BODY
 }
 
 #Preview {
     RestaurantDetailsView(restaurant: .init(id: 0, name: "Japan's Finest Tapas", imageName: "tapas"))
 }
+
---
+++ TravelDiscoverSwiftUI/Utils/StarsView.swift
@@ -0,0 +1,64 @@
+//
+//  StarsView.swift
+//  TravelDiscoverSwiftUI
+//
+//  Created by Rustam Keneev on 18/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct StarsView: View {
+    let rating: CGFloat
+    let maxRating: CGFloat
+    
+    private let size: CGFloat = 18
+    var body: some View {
+        let text = HStack(spacing: 0) {
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: size, height: size, alignment: .center)
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: size, height: size, alignment: .center)
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: size, height: size, alignment: .center)
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: size, height: size, alignment: .center)
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: size, height: size, alignment: .center)
+        }
+
+        ZStack {
+            text
+            HStack(content: {
+                GeometryReader(content: { geometry in
+                    HStack(spacing: 0, content: {
+                        let width1 = self.valueForWidth(geometry.size.width, value: rating)
+                        let width2 = self.valueForWidth(geometry.size.width, value: (maxRating - rating))
+                        Rectangle()
+                            .frame(width: width1, height: geometry.size.height, alignment: .center)
+                            .foregroundColor(.yellow)
+                        
+                        Rectangle()
+                            .frame(width: width2, height: geometry.size.height, alignment: .center)
+                            .foregroundColor(.gray)
+                    })
+                })
+                .frame(width: size * maxRating, height: size, alignment: .trailing)
+            })
+            .mask(
+                text
+            )
+        }
+        .frame(width: size * maxRating, height: size, alignment: .leading)
+    }
+    
+    func valueForWidth(_ width: CGFloat, value: CGFloat) -> CGFloat {
+        value * width / maxRating
+    }
+}
+
